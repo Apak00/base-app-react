6a7b707802aa22d555cb7c24f14736948bd08fe7
Mahmud Apak <apakmahmud@gmail.com>
2021-05-10 21:43:30 +0300

    Change file structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(js|mjs|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['@babel/preset-env', { targets: "defaults" }]
-                        ],
-                        plugins: ['@babel/plugin-transform-react-jsx']
-                    }
-                }
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: ["ts-loader"],
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-                use: ["file-loader"],
-            },
-        ]
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "index.html"),
-        }),
+  entry: './index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: ['@babel/plugin-transform-react-jsx'],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx', '.tsx', '.ts']
-    }
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts'],
+  },
 };
